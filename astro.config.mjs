5321d85e7ffcec272d1cc7cee677e68aaf44885a
root <root@HY>
2026-03-22 07:17:57 +0100

    fix: update site domain to bcd.moe

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.saroprock.com",
+  site: "https://www.bcd.moe",
   output: "server",
   adapter: vercel(),
   vite: {
